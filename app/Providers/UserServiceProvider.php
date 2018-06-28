65bd711025c491611ba0efd97f1e5361c6253b25
xiaotong.li <xiaotong.li@sh.fieldschina.com>
2018-06-28 18:23:27 +0800

    UserServiceProvider 大小写修正

--- app/Providers/UserServiceProvider.php
+++ app/Providers/UserServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class userServiceProvider extends ServiceProvider
+class UserServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap the application services.
