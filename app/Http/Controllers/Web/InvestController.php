f6635e1fd400a3b0bbceb2ee5d522f6d5348f0c7
ahulxt <524216645@qq.com>
2018-07-15 20:35:58 +0800

    investController rename to InvestController

--- app/Http/Controllers/Web/InvestController.php
+++ app/Http/Controllers/Web/InvestController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Web;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class investController extends Controller {
+class InvestController extends Controller {
 	
 	public $data = [];
 	
