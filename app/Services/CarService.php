d39467f8cc82366548ccc67296e81e357a6f17db
ahulxt <524216645@qq.com>
2018-08-04 15:33:33 +0800

    车牌号码验证

--- app/Services/CarService.php
+++ app/Services/CarService.php
@@ -445,11 +445,7 @@ class CarService extends BaseService {
 		if (empty($post['plate_number'])) {
 			json_msg(trans('validation.required', ['attr' => trans('common.plate_number')]), 40001);
 		} else {
-			$province     = mb_substr($post['plate_number'], 0, 1, 'utf8');
-			$plateNumber  = mb_substr($post['plate_number'], 1, 6, 'utf8');
-			$provinceList = array_column($this->getProvince(), 'name');
-			
-			if (!in_array($province, $provinceList) || !preg_match(config('project.PATTERN.PLATE'), $plateNumber)) {
+			if (!preg_match(config('project.PATTERN.PLATE'), $post['plate_number'])) {
 				json_msg(trans('validation.invalid', ['attr' => trans('common.plate_number')]), 40003);
 			}
 		}
