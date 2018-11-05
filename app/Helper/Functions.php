64d9cac27d0ee21c5ba782eabea5250188aaf8a3
ahulxt <524216645@qq.com>
2018-11-05 20:28:13 +0800

    微信支付 part3

--- app/Helper/Functions.php
+++ app/Helper/Functions.php
@@ -364,6 +364,7 @@ function currencyFormat($value) {
  */
 function request_post($url = '', $post_data = '') {
 	
+	$url = trim($url);
 	if (empty($url)) return false;
 	
 	if (is_array($post_data)) $post_data = http_build_query($post_data);
@@ -371,7 +372,7 @@ function request_post($url = '', $post_data = '') {
 	$ch = curl_init();//初始化curl
 	curl_setopt($ch, CURLOPT_URL, $url);//抓取指定网页
 	curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
-//	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
 	curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
 	$data = curl_exec($ch);//运行curl
